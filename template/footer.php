550422b78b3e6f0b185d4eb70da122f5ede633d4
Rahmatkausar <kausarskr992004@gmail.com>
2025-05-24 15:06:58 +0700

    First commit

---
+++ template/footer.php
@@ -0,0 +1,14 @@
+</div>
+<style>
+    .copyright {
+        padding: 50px;
+    }
+</style>
+<footer style="margin-top:100px;color:white" class="sticky-footer bg-success">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Sistem Informasi Kost 2025 | Kelompok 2</span>
+        </div>
+    </div>
+</footer>
+</body>
