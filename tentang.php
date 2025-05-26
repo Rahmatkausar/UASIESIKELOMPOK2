70aaf38519c7b23807a7e9c66731156cc5787d22
Rahmatkausar <kausarskr992004@gmail.com>
2025-05-26 11:36:18 +0700

    Update

--- tentang.php
+++ tentang.php
@@ -4,7 +4,7 @@ include 'template/header.php'
 ?>
 <div class="container">
 
-    <h5>Tentang Simkos</h5>
+    <h5>Tentang Easy kos</h5>
     <hr>
     <p>Simkos adalah website yang menyajikan informasi kamar kost dengan fasilitas kost, harga dan foto kamar yang sedetail mungkin
         seperti aslinya, anda bisa mendaftar sebagai pencari kost ataupun penyedia kost-kostsan
